034ccdf447cc1eb52aa6bd551dc6213db828c178
Felipe Santos <santos.flip@gmail.com>
2019-02-25 23:14:49 -0300

    adding some texts

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ final Color dark = HexColor('#19171C');
 final Color purpleBackground = HexColor('#1D1A24');
 final Color purpleBottom = HexColor('#2F2942');
 final Color purpleCard = HexColor('#25212E'); // top
+final Color tagBackground = HexColor('#353239');
 
 class MyApp extends StatelessWidget {
   @override
@@ -35,9 +36,16 @@ class StreamCard extends StatelessWidget {
       child: Container(
         height: 350.0,
         decoration: BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.black87,
+                offset: Offset(-1.0, 2.0),
+                blurRadius: 80.0,
+              ),
+            ],
             color: purpleCard,
             borderRadius: BorderRadius.all(Radius.circular(5))),
-        child: Row(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[StreamCardImage(), StreamCardInfo()],
@@ -54,7 +62,85 @@ class StreamCardInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, top: 24.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 60.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                    color: Colors.white24,
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text('ESL_CSGO',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            decoration: TextDecoration.none)),
+                    Text('LIVE: FaZe Clan VS Cloud9 [Cache] - IEM Katowice',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            decoration: TextDecoration.none)),
+                    Text('Counter Strike - Global Offensive',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            decoration: TextDecoration.none)),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                child: Container(
+                  color: tagBackground,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('English',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            decoration: TextDecoration.none)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 16.0, top: 16.0, bottom: 16.0),
+                child: Container(
+                  color: tagBackground,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Map: Dust II',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            decoration: TextDecoration.none)),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -78,6 +164,7 @@ class StreamCardImage extends StatelessWidget {
           ),
           Positioned(
             bottom: 12.0,
+            left: 16.0,
             child: StreamCardCountViewers(),
           )
         ],
@@ -98,7 +185,7 @@ class StreamCardCountViewers extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         Padding(
-          padding: const EdgeInsets.only(left: 12.0, top: 2.0),
+          padding: const EdgeInsets.only(top: 2.0),
           child: Container(
             width: 12.0,
             height: 12.0,
